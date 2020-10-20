3f0592c1e55718ffe51b982c5f6f5d47df9adb7a
gverebl <gverbel@LAPTOP-9LPSFRC2.localdomain>
2020-10-20 03:01:06 -0500

    Delete update and count creates

--- workHours/commands.py
+++ workHours/commands.py
@@ -1,4 +1,6 @@
+from os import sep
 import click
+from click.termui import prompt
 from tabulate import tabulate
 #import uuid
 
@@ -126,34 +128,193 @@ def delete(ctx, record_id):
     """
         Delete an our based on argument id
     """
-    # hour_service = HourService(ctx.obj['work_hours_table'])
+    hour_service = HourService(ctx.obj['work_hours_table'])
+    deleted = hour_service.delete_hour(record_id)
+    if not deleted:
+        click.echo(click.style('ERROR', bg='red'))
+        click.echo('Operation error:')
+        click.echo('No record found with the id you input.')
+    elif len(deleted) == 1:
+        click.echo(click.style('SUCCESS', bg='green'))
+        click.echo(f'The record with id {deleted[0]} was successfully deleted.')
+    else:
+        click.echo(click.style('ERROR', bg='red'))
+        click.echo('Operation error:')
+        click.echo('There are more than one record that match your input.')
+        click.echo(f'Try using the command <tracker hours uuid {record_id}> to get the specific id you want')
+        
+        
 
-    # #Verificamos si el id el correcto
-    # hour_list = hour_service.get_hours()
+
+    
 
 #update
 @hour.command()
+@click.argument('record_id', type=str)
+@click.option(
+    '-y', '--year',
+    type=int,
+    help='Year to update hours in'
+)
+@click.option(
+    '-w', '--week',
+    type=int,
+    help='Week to update hours in'
+)
+@click.option(
+    '-d', '--day',
+    type=int,
+    help='Day to update hours in'
+)
+@click.option(
+    '-h', '--hours',
+    type=int,
+    help='Hour to update hours in'
+)
+@click.option(
+    '-m', '--minutes',
+    type=int,
+    help='Minutes to update hours in'
+)
+@click.option(
+    '-l', '--description',
+    type=str,
+    help='Minutes to update hours in'
+)
 @click.pass_context
-def update(ctx):
+def update(ctx, record_id, year=None, month=None, day=None, week=None, hours=None, minutes=None, description=None  ):
     """
         Updates a specific record based on id
     """
-    pass
+    hour_service = HourService(ctx.obj['work_hours_table'])
+
+    update_options = {
+        'year': year,
+        'month': month,
+        'week': week,
+        'day': day,
+        'hours': hours,
+        'minute': minutes,
+        'description': description
+    }
+
+    updated = hour_service.update_hour(record_id, update_options)
+    if not updated:
+        click.echo(click.style('ERROR', bg='red'))
+        click.echo('Operation error:')
+        click.echo('No record found with the id you input.')
+    elif len(updated) == 1:
+        click.echo(click.style('SUCCESS', bg='green'))
+        click.echo(f'The record with id {updated[0]} was successfully updated.')
+    else:
+        click.echo(click.style('ERROR', bg='red'))
+        click.echo('Operation error:')
+        click.echo('There are more than one record that match your input.')
+        click.echo(f'Try using the command <tracker hours uuid {record_id}> to get the specific id you want')
+
 
 # count
 @hour.command()
+@click.option('--all', is_flag=True)
+@click.option(
+    '-w', '--week',
+    type = int,
+    help='Week to count the hours.'
+)
+@click.option(
+    '-i', '--start',
+    type = int,
+    help='From which week to start counting'
+)
+@click.option(
+    '-f', '--stop',
+    type = int,
+    help='Until which week to count'
+)
+
 @click.pass_context
-def count(ctx):
+def count(ctx, all, week, start, stop):
     """
         Prints the sum of all hours in a week and generates
         a report based on the description of all days
             default: current
             options: 
                 --week=n : For the nth week
-                --multiweek=[n,m] : For the period of week n to m
                 --all : For all the records
+                --start : from what week to start. 
+                    (If no --to , then from that week to end)
+                --stop : to what week to count 
+                    (If no --from, then from begining to --to)
     """
-    pass
+    
+    params = {'all': False, 'start': None, 'stop': None}
+    if all:
+        params['all'] = True
+    else:
+        if week:
+            params['start'] = week
+            params['stop'] = week
+        else:
+            if start:
+                params['start'] = start
+            if stop: 
+                params['stop'] = stop
+    
+    hour_service = HourService(ctx.obj['work_hours_table'])
+    
+    table, work_time, report = hour_service.count_hours(**params)
+    
+    print('Period of work')
+    hour_table = map( 
+        lambda x: [x[0][:8]] + x[1:-1], # Shotens id and eliminates date_created
+            table)
+    hour_table = list(hour_table)
+    print(tabulate(hour_table, headers=Hour.schema()[:-1]))
+
+    print('\nTotal time worked: ')
+    print(f'\t\t\t\tHours: {work_time[0]}\tMinutes: {work_time[1]}')
+
+    print('\nActivities performed')
+    print(*report, sep=', ')
+
+
+# get uuid
+@hour.command()
+@click.argument('uuid_segment', type=str)
+@click.pass_context
+def uuid(ctx, uuid_segment):
+    """ Returns the complete uuid from the first 4 digits """
+    hour_service = HourService(ctx.obj['work_hours_table'])
+    
+    ids = hour_service.get_uuid(uuid_segment)
+
+    if ids:
+
+        if len(ids) > 1:
+            click.echo('There are multiple possible ids, choose one to pipe with update or delete: ')
+            for i, idx in enumerate(ids):
+                click.echo(f'{i}. {idx}')
+            option=click.prompt('Option', type=int)
+            shortest_id = _get_short_uuid(option, ids)
+            click.echo(f'The shortest id possible is {shortest_id}')
+
+        else:
+            click.echo(ids[0])
+    else:
+        click.echo('No id found to match that pattern....')
+
+def _get_short_uuid(option, uuids):
+    """Returns the shortes possible uuid when more than one is possible"""
+    top_len = 0
+    for elems in zip(*uuids):
+        if len(set(elems)) > 1:
+            return uuids[option][:top_len+1]
+        else:
+            top_len += 1
+    return uuids[option]
+
+
+
 
 # Current time
 @hour.command()
--- workHours/services.py
+++ workHours/services.py
@@ -9,7 +9,15 @@ class HourService:
 
     def __init__(self, table):
         self.table = table
+
+    def __read_hours(self):
+        """Returns the records dictionaries"""
+        with open(self.table, mode='r') as f:
+            data_dict = csv.DictReader(f, fieldnames=Hour.schema())
+            return [elem for elem in data_dict]
     
+
+
     # Adds an hour record
     def add_hour(self, work_hour):
         with open(self.table, mode='a') as f:
@@ -41,12 +49,10 @@ class HourService:
 
 
         # Data retrieved
-        with open(self.table, mode='r') as f:
-            data_dict = csv.DictReader(f, fieldnames=Hour.schema())
-            data = []
-            
-            for elem in data_dict:
-                data.append(list(elem.values()))
+        data_dict = self.__read_hours()
+        data = [] 
+        for elem in data_dict:
+            data.append(list(elem.values()))
         
         
         #Data filtering 
@@ -60,6 +66,157 @@ class HourService:
             ]
             
     
+    #Deletes a record
+    def delete_hour(self, uid):
+        """Deletes a record specified by user's input"""
+        def delete(records, uid):
+            kept = []
+            for record in records:
+                if record['id'] != uid:
+                    kept.append(record)
+            
+            return kept
+                    
+        records = self.__read_hours()
+        possible_delete = []
+        for record in records:
+            if uid in record['id']:
+                possible_delete.append(record['id'])
+
+        if not possible_delete: #empty
+            return None
+        elif len(possible_delete) > 1:
+            return possible_delete
+        else:
+            kept_data = delete(records, possible_delete[0])
+            self._save_to_disk(kept_data)
+            return possible_delete
+
+    #Update a recors
+    def update_hour(self, record_id, options):
+        def update(records, record_to_update, year, month, week, day, minute, hours, description):
+            new_data = []
+            for record in records:
+                if record['id'] == record_to_update:
+                    #Aqui hacer el update
+                    new_record = record.copy()
+                    if year:
+                        new_record['year'] = year
+                    if month:
+                        new_record['month'] = month
+                    if week:
+                        new_record['week'] = week
+                    if day:
+                        new_record['day'] = day
+                    if hours:
+                        new_record['hours'] = hours
+                    if minute:
+                        new_record['minute'] = minute
+                    if description:
+                        new_record['description'] = description
+                    new_data.append(new_record)
+                else:
+                    new_data.append(record)
+            return new_data
+
+        records = self.__read_hours()
+        possible_update = []
+        for record in records:
+            if record_id in record['id']:
+                possible_update.append(record['id'])
+       
+        if not possible_update: #empty
+            return None
+        elif len(possible_update) > 1:
+            return possible_update
+        else:
+            new_data = update(records, possible_update[0], **options)
+            self._save_to_disk(new_data)
+            return possible_update            
+
+
+    def _save_to_disk(self, hours):
+        tmp_table_name = self.table + '.tmp'
+        with open(tmp_table_name, mode='w') as f:
+            writer = csv.DictWriter(f, fieldnames=Hour.schema())
+            writer.writerows(hours)  
+        os.remove(self.table)
+        os.rename(tmp_table_name, self.table)        
+
+
+    #Returns the uuid from at least 4 characcters
+    def get_uuid(self, uuid_segment):
+        """Returns complete uuid from segment"""
+        records = self.__read_hours()
+
+        possible_ids = []
+        
+        for record in records:
+            if uuid_segment in record['id']:
+                possible_ids.append(record['id'])
+        
+        return possible_ids
+
+    
+    # Counts all hours for a fixed amount of weeks
+    def count_hours(self, all, start, stop):
+        """ Returns a report for the weeks selected.
+            This command can only be used within a year unless all is used."""
+        
+        current_time = self.get_current_time()
+        # Interpreting the parameters
+        if start and not stop:
+            stop = current_time['week']
+        if stop and not start:
+            start = 0
+        
+        if not start and not stop:
+            start = current_time['week']
+            stop = current_time['week']
+
+        # Getting the data
+        data = self.__read_hours()
+
+        # Filtering data from params 
+        if all:
+            filtered_data = data
+        else:
+            filtered_data = []
+            for record in data:
+                if int(record['week']) >= start and int(record['week']) <= stop:
+                    filtered_data.append(record)
+        
+
+        #Transfrom data to print
+        data_print = [] 
+        for elem in filtered_data:
+            data_print.append(list(elem.values()))
+
+        # Count hours and get descriptions
+        hours = 0 
+        minutes = 0 
+        descriptions = []
+        for record in filtered_data:
+            hours += int(record['hours'])
+            minutes += int(record['minutes'])
+            descriptions.append(record['description'])
+
+        #Fix for extra minutes
+        hours += minutes//60
+        minutes = minutes%60 
+    
+        # Resulting time 
+        worked_time = (hours, minutes)
+
+
+        return data_print, worked_time, descriptions
+
+
+            
+
+
+        
+
     @staticmethod
     def get_current_time():
         return {
